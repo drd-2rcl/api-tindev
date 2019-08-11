30d261de134c642ba2b4d324eddfab57abf517ef
Diogo <drd.rclrd@gmail.com>
2019-08-10 23:43:43 -0300

    was added db

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-fjiio.mongodb.net/omnistack?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 server.use(express.json());
 server.use(routes);
